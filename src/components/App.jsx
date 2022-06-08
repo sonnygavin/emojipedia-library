3d59d72309e373a138a26b5aee1d794ae67e4cc1
Gavin Waako <102330724+sonnygavin@users.noreply.github.com>
2022-06-08 13:29:48 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Entry from "./Entry";
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Entry />
+    </div>
+  );
+}
+
+export default App;
